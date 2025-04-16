5af4552a130264d58a07d27e7d7638e717ffb79a
weiichen2001 <weiichen2001@gmail.com>
2025-04-16 15:44:55 +0800

    0416-1

--- src/0416/App-0416-1.jsx
+++ src/0416/App-0416-1.jsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 
 function App() {
 
+    // 使用useState建立變數
     const [citys, setCitys] = useState([])
 
     // 渲染後，使用useEfffect先取得天氣資料
@@ -45,27 +46,68 @@ function App() {
                                 <div className="card text-center">
                                     {/* 頭:卡片標題 */}
                                     <div className="card-header">
-                                        <div className="h4 my-0">台北市</div>
+                                        <div className="h4 my-0">
+                                            {/* 台北市 */}
+                                            {city.locationName}
+                                        </div>
                                     </div>
                                     {/* 身:卡片內容 */}
                                     <div className="row row-cols-3">
-                                        {/* 第一欄 */}
-                                        <div className="col">
-                                            <div className="h4 my-0">16日</div>
-                                            {/* 時間 */}
-                                            上午6:00 <br />
-                                            ~<br />
-                                            下午6:00 <br />
-                                            {/* 天氣圖 使用執行路徑所以放在poblic資料夾中 如果放在其他資料夾都要寫imprt */}
-                                            <img src="./weatherIcon/晴時多雲.svg" alt="" />
-                                            {/* 天氣名稱 */}
-                                            <div className="mt-2">晴時多雲</div>
-                                            {/* 降雨機率 */}
-                                            <div className="mt-2">
-                                                {/* icon */}
-                                                <i className="bi bi-umbrella">10%</i>
-                                            </div>
-                                        </div>
+                                        {/* 顯示每個縣市的3個欄位資訊 */}
+                                        {
+                                            city.weatherElement[0].time.map((item, index) => {
+                                                return (
+                                                    <div className="col" key={index}>
+                                                        <div className="h4 my-0">
+                                                            {/* 16日 */}
+                                                            {/* 使用日期時間函數 toLocaleString()， */}
+                                                            {
+                                                                new Date(item.startTime).toLocaleString(undefined, {
+                                                                    day: 'numeric'
+                                                                })
+                                                            }
+                                                        </div>
+                                                        {/* 時間
+                                                        上午6:00 <br />
+                                                        ~<br />
+                                                        下午6:00 <br /> */}
+                                                        {
+                                                            new Date(item.startTime).toLocaleString(undefined, {
+                                                                hour: 'numeric',
+                                                                minute: 'numeric'
+                                                            })
+                                                        }
+                                                        <br/>~<br />
+                                                        {
+                                                            new Date(item.endTime).toLocaleString(undefined, {
+                                                                hour: 'numeric',
+                                                                minute: 'numeric'
+                                                            })
+                                                        }
+                                                        <br/>
+                                                        {/* 天氣圖 使用執行路徑所以放在poblic資料夾中 如果放在其他資料夾都要寫imprt */}
+                                                        {/* <img src="./weatherIcon/晴時多雲.svg" alt="" /> */}
+                                                        <img src={`./weatherIcon/${item.parameter.parameterName}.svg`} alt="" />
+                                                        {/* 天氣名稱 */}
+                                                        <div className="mt-2">
+                                                            {/* 晴時多雲 */}
+                                                            {item.parameter.parameterName}
+                                                        </div>
+                                                        {/* 降雨機率 */}
+                                                        <div className="mt-2">
+                                                            {/* icon */}
+                                                            <i className="bi bi-umbrella">
+                                                                {/* 10% */}
+                                                                {city.weatherElement[4].time[index].parameter.parameterName}%
+                                                            </i>
+                                                        </div>
+                                                    </div>
+
+                                                )
+
+                                            })
+                                        }
+
                                     </div>
                                 </div>
                             </div>
